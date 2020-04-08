a38338bd0e0d34c87573fb4ffe88b803774afa34
Vitaliy Vit <vitalik.vit37@gmail.com>
2020-04-08 20:38:38 +0300

    Gender Class filling

--- Fitness.BL/Model/Gender.cs
+++ Fitness.BL/Model/Gender.cs
@@ -13,5 +13,59 @@ namespace Fitness.BL.Model
     /// </summary>
     class Gender
     {
+        #region ---===   Property   ===---
+
+        /// <summary>
+        /// 
+        /// Название
+        /// 
+        /// </summary>
+        public string Name { get; }
+
+        #endregion
+
+        #region ---===   Ctor   ===---
+
+        /// <summary>
+        /// 
+        /// Создать новый пол.
+        /// 
+        /// </summary>
+        /// 
+        /// <param name="name">
+        /// 
+        /// Имя пола.
+        /// 
+        /// </param>
+        public Gender(string name)
+        {
+            Validation(name);
+
+            Name = name;
+        }
+
+        #endregion
+
+        #region ---===   Private Method   ===---
+
+        private void Validation(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("Имя пола не может быть пустым!", nameof(name));  //TODO: создать свое исключение
+            }
+        }
+
+        #endregion
+
+        #region ---===   Override Method   ===---
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+        #endregion
+
     }
 }
